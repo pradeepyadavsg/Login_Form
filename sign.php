d51af13664f5c15467049d4f97e27bfba5feacbf
Pradeep Kumar Yadav <prshy123@gmail.com>
2024-08-11 18:51:58 +0530

    Last Commit

--- sign.php
+++ sign.php
@@ -2,12 +2,14 @@
 
 $success = 0;
 $user = 0;
+$invalid=0;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include 'connect.php';
 
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $cpassword=$_POST['cpassword'];
 
 
     $sql = "select * from `registration` where username='$username'";
@@ -15,20 +17,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($result) {
         $num = mysqli_num_rows($result);
         if ($num > 0) {
-            // echo "User already exist";
+           
             $user = 1;
         } else {
+            if($password===$cpassword){
+
+          
             $sql = "insert into `registration`(username,password) values('$username','$password')";
 
             $result = mysqli_query($con, $sql);
             if ($result) {
-                // echo "Signup Successfully";
+               
                 $success = 1;
-                header('location:login.php');
+            }    header('location:login.php');
             } else {
-                die(mysqli_error($con));
+               $invalid=1;
             }
-        }
+        
+    }
     }
 }
 ?>
@@ -53,6 +59,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
     ?>
     <?php
+    if ($invalid) {
+        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Oh- Sorry! </strong> Password did not match.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+    }
+    ?>
+    <?php
     if ($success) {
         echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
   <strong>Yes-Great! </strong>Sugnup Successfully.
@@ -71,6 +85,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" placeholder="Enter your password" name="password">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" placeholder="Enter your Confirm password" name="cpassword">
             </div>
             <button type="submit" class="btn btn-primary w-100">Sign Up</button>
         </form>
